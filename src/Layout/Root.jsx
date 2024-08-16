8f017c16e831a9b0e4cf3471ac2e07fb2cf8e12e
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-08-16 23:25:36 +0600

    added navbar

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
-
+import Navbar from "../component/Navbar";
 
 const Root = () => {
-    return (
-        <div className="max-w-[1440px] w-11/12 lg:w-10/12 mx-auto">
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <>
+    <div className="bg-base-100 shadow-xl fixed top-0 w-full">
+      <Navbar></Navbar>
+    </div>
+      <div className="max-w-[1440px] mt-24 w-11/12 lg:w-10/12 mx-auto">
+        <Outlet></Outlet>
+      </div>
+    </>
+  );
 };
 
 export default Root;
