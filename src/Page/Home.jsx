c4bd806508becb5ac86c7411a9b5c2650ebd1afe
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-08-17 12:00:36 +0600

    added category functionlity

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -12,9 +12,10 @@ const Home = () => {
     const [count, setCount] = useState(0);
     const [search, setSearch] = useState("");
     const [searchText, setSearchText] = useState("");
-
+    const [category, setCategory] = useState("");
     const numberOfPages = Math.ceil(count / itemsPerPage);
  
+    console.log(category);
     
     const pages = [
       ...Array(numberOfPages)
@@ -22,9 +23,9 @@ const Home = () => {
         .map((element) => element + 1),
     ];
     const { data: productsData = [], isLoading } = useQuery({
-        queryKey: ["productsData" ,  currentPage, search],
+        queryKey: ["productsData" ,  currentPage, search , category],
         queryFn: () =>
-            axios.get(`http://localhost:3000/products?page=${currentPage}&size=${itemsPerPage}&search=${search}`).then((res) => {
+            axios.get(`http://localhost:3000/products?page=${currentPage}&size=${itemsPerPage}&search=${search}&category=${category}`).then((res) => {
             
             return res.data;
           }),
@@ -91,8 +92,35 @@ if (isLoading) return <div className="flex justify-center items-center h-screen"
           >
             Reset
           </AwesomeButton>
-          {/* <button onClick={handleReset} className='btn btn-outline text-white bg-blue-500'>Reset</button> */}
         </div>
+            <div>
+            <select
+          name="category"
+          onChange={(e) => setCategory(e.target.value)}
+          id="category"
+          value={category}
+          className="border p-2 rounded-md"
+        >
+          <option value="">All Product</option>
+          <option value="Electronics">Electronics</option>
+          <option value="Wearables">Wearables</option>
+          <option value="Audio">Audio</option>
+          <option value="Computers">Computers</option>
+          <option value="Accessories">Accessories</option>
+          <option value="Gaming">Gaming</option>
+          <option value="Photography">Photography</option>
+          <option value="Smart Home">Smart Home</option>
+          <option value="Transportation">Transportation</option>
+          <option value="Lighting">Lighting</option>
+          <option value="Home Appliances">Home Appliances</option>
+          <option value="Personal Care">Personal Care</option>
+          <option value="Home Entertainment">Home Entertainment</option>
+          <option value="Kitchen">Kitchen</option>
+          <option value="Security">Security</option>
+          <option value="Health">Health</option>
+        </select>
+            </div>
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-10">
             {
                 productsData.map((product , index)=> <ProductCard key={index} product={product}></ProductCard>)
